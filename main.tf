3f811e9283dd0c75e244d766d18f6e5214fb3784
nagmat <nagmat@gmail.com>
2024-07-12 13:17:48 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_budgets_budget" "minbudget" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "3"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2024-07-01_15:00"
+}
